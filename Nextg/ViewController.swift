2e813caea01d7c3a71832b0a8143347a47950ef6
solve0919 <saltbitcoin@gmail.com>
2019-06-05 00:10:34 +0900

    Initial Commit

---
+++ Nextg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nextg
+//
+//  Created by 塩田　祐也 on 2019/06/05.
+//  Copyright © 2019 塩田　祐也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NextgTests/NextgTests.swift
@@ -0,0 +1,34 @@
+//
+//  NextgTests.swift
+//  NextgTests
+//
+//  Created by 塩田　祐也 on 2019/06/05.
+//  Copyright © 2019 塩田　祐也. All rights reserved.
+//
+
+import XCTest
+@testable import Nextg
+
+class NextgTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
