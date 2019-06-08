69ceb8a167ed128605d3af2769399751849ff87f
solve0919 <saltnem@gmail.com>
2019-06-08 13:38:33 +0900

    add +

--- Nextg/ViewController.swift
+++ Nextg/ViewController.swift
@@ -13,13 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+//  ロード時の処理
         formlabel.text = ""
         answerlabel.text = ""
-//        ロード時の処理
     }
     
     @IBAction func inputformla(_ sender: UIButton) {
-// 飛んできた文字を表示させる
+
+        // 前回分を表示させる
         guard let formlatext = formlabel.text else {
             return
         }
@@ -27,7 +28,7 @@ class ViewController: UIViewController {
         guard let senderedText = sender.titleLabel?.text else {
             return
         }
-//        もともとの数字に送った数字を足す
+//     もともとの数字に送った数字を足す
         formlabel.text = formlatext + senderedText
     }
     @IBAction func clearcalculation(_ sender: UIButton) {
@@ -40,10 +41,42 @@ class ViewController: UIViewController {
         guard let formlatext = formlabel.text else {
             return
         }
-//        let formula: String = formatFormula(formulatext)
-//        answerlabel.text = evalFormula(formula)
+        let formula: String = formatFormula(formlatext)
+        answerlabel.text = evalFormula(formula)
     }
-    
-    
+}
+
+private func formatFormula(_ formula: String) -> String {
+    // 入力された整数には`.0`を追加して小数として評価する
+    // また`÷`を`/`に、`×`を`*`に置換する
+    let formattedFormula: String = formula.replacingOccurrences(
+        of: "(?<=^|[÷×\\+\\-\\(])([0-9]+)(?=[÷×\\+\\-\\)]|$)",
+        with: "$1.0",
+        options: NSString.CompareOptions.regularExpression,
+        range: nil
+        ).replacingOccurrences(of: "÷", with: "/").replacingOccurrences(of: "×", with: "*")
+    return formattedFormula
+}
+
+private func evalFormula(_ formula: String) -> String {
+    do {
+        // Expressionで文字列の計算式を評価して答えを求める
+        let expression = Expression(formula)
+        let answer = try expression.evaluate()
+        return formatAnswer(String(answer))
+    } catch {
+        // 計算式が不当だった場合
+        return "式を正しく入力してください"
+    }
+}
+
+private func formatAnswer(_ answer: String) -> String {
+    // 答えの小数点以下が`.0`だった場合は、`.0`を削除して答えを整数で表示する
+    let formattedAnswer: String = answer.replacingOccurrences(
+        of: "\\.0$",
+        with: "",
+        options: NSString.CompareOptions.regularExpression,
+        range: nil)
+    return formattedAnswer
 }
 
